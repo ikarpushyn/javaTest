7fa2c0feb75d90768649217b7abba299256a4de4
Ilija <komperdel@gmail.com>
2024-11-15 17:53:45 +0200

    first commits

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
         Person user1 = new Person();
         Person user2 = new Person("Ilya");
-        Person user3 = new Person("Ilya", 100);
+        Person user3 = new Person("Ilya", 29);
         Person user4 = new Person("Ilya", "Karpushyn");
 
         System.out.println(user1.name);
